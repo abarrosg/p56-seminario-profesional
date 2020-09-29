5d0b50e4be18017c8a75ba3c139b1826c85e2378
Anthony Barros <abarrosg@est.ups.edu.ec>
2020-09-29 17:31:29 -0500

    Se separa la parte de klas respuesta por parte del servidor

--- code/node-fundamentals/01_server/server.js
+++ code/node-fundamentals/01_server/server.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
+const response = require('./network/response')
 
 const router = express.Router()
 
@@ -9,16 +10,14 @@ app.use( bodyParser.urlencoded( {extended:false} ) )
 app.use( router )
 
 router.get('/carrera', function(req,res){
-   console.log( req.header, req.header['authorization'])
-
-   res.send( 'Lista de Carreras de la UPS.' )
-})
+    response.success( req, res, 'Lista de Carreras de la UPS.' , 200 )
+   })
 
 router.post('/carrera', function(req,res){
     if( req.query.error == 'ok'){
-        res.status(500).send( { tipo_error:1, mensaje_error:'Error en el Servidor', mensaje_exito: '' } )
+        response.error( req, res, 'Error al Ingresar la carrera.', 500 )
      } else{
-        res.status(200).send( { tipo_error:0, mensaje_error:'', mensaje_exito: 'Todo Ok' } )
+        response.success( req, res, 'Ingreso de carrera exitoso.' , 201 )
      }
 })
 
