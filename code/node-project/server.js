be00432e9dcabff0fcdc5e65a702223e709eb191
Anthony Barros <abarrosg@est.ups.edu.ec>
2020-09-30 15:48:38 -0500

    CRUD en javascript sin persistencia

--- code/node-project/server.js
+++ code/node-project/server.js
@@ -1,27 +1,15 @@
-const express = require ('express')
+const express = require('express')
 const bodyParser = require('body-parser')
 const response = require('./network/response')
 
-const router = express.Router()
+const router = require('./network/routes')
 
 var app = express()
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded( {extended:false} ) )
-app.use( router )
+router( app )
 
-router.get('/carrera', function(req,res){
-    response.success( req, res, 'Lista de Carreras de la UPS.' , 200 )
-   })
-
-router.post('/carrera', function(req,res){
-    if( req.query.error == 'ok'){
-        response.error( req, res, 'Error al Ingresar la carrera.', 500 )
-     } else{
-        response.success( req, res, 'Ingreso de carrera exitoso.' , 201 )
-     }
-})
-
-app.use('/', express.static('public') )
+app.use( '/', express.static('public') )
 
 app.listen( 5000 )
-console.log( 'La aplicacion esta escuchando en http://localhost:5000' )
+console.log( 'La aplicación está escuchando en http://localhost:5000' )
